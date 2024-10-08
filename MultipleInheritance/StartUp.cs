0a586db1751165cebded752da6af25d2643b6973
YordanSoftDev <yordan_georgiev@aol.com>
2024-10-08 18:01:10 +0300

    Using inheritance
    Tasks from topic C Sharp OOP - Inheritance

---
+++ MultipleInheritance/StartUp.cs
@@ -0,0 +1,14 @@
+﻿namespace Farm
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            Dog dog = new();
+            Puppy puppy = new();
+            puppy.Weep();
+            puppy.Bark();
+            dog.Eat();
+        }
+    }
+}
---
+++ SingleInhritance/StartUp.cs
@@ -0,0 +1,11 @@
+﻿namespace Farm
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            Dog dog = new();
+            dog.Eat();
+        }
+    }
+}
