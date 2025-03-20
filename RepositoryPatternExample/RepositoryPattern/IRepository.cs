cded499fdd85a9edbd6a2a556bbeaefacfb30a13
YordanSoftDev <168276511+YordanSoftDev@users.noreply.github.com>
2025-03-20 19:19:23 +0200

    Repository pattern example

--- RepositoryPatternExample/RepositoryPattern/IRepository.cs
+++ RepositoryPatternExample/RepositoryPattern/IRepository.cs
@@ -5,10 +5,15 @@ namespace RepositoryPattern
     public interface IRepository<T> where T : class
     {
         IReadOnlyCollection<T> Models { get; }
+        
         void AddNew(T model);
+        
         T GetByName(string name);
+        
         bool Exists(string name);
+        
         void Update(T model);
+        
         void Remove(string name);
     }
 }
