6aa6d25fbe9f9d35641a3ab8036a022fd9eb5527
YordanSoftDev <168276511+YordanSoftDev@users.noreply.github.com>
2025-07-16 17:43:13 +0300

    Influencer Manager Application with classes and interfaces "services"

--- C# OOP - Exams/InfluencerManagerApp/Services/Contracts/ICampaignService.cs
+++ C# OOP - Exams/InfluencerManagerApp/Services/Contracts/ICampaignService.cs
@@ -3,7 +3,9 @@
     public interface ICampaignService
     {
         public string BeginCampaign(string typeName, string brand);
+        
         public string FundCampaign(string brand, double amount);
+        
         public string CloseCampaign(string brand);
     }
 }
