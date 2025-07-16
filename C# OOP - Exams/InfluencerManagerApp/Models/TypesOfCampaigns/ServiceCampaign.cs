40740b03b1446527e31f1f7f4761b7e3121a45ef
YordanSoftDev <168276511+YordanSoftDev@users.noreply.github.com>
2025-07-16 17:40:04 +0300

    Influencer Manager Application with classes and interfaces "services"

--- C# OOP - Exams/InfluencerManagerApp/Models/TypesOfCampaigns/ServiceCampaign.cs
+++ C# OOP - Exams/InfluencerManagerApp/Models/TypesOfCampaigns/ServiceCampaign.cs
@@ -4,7 +4,7 @@ namespace InfluencerManagerApp.Models.TypesOfCampaigns
     public class ServiceCampaign : Campaign
     {
         private const double fixedBudget = 30000;
-        public ServiceCampaign(string brand) : base(brand, fixedBudget)
+        public ServiceCampaign(string brand) : base(brand, fixedBudget) 
         {
         }
     }
